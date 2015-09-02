fc55f8182018eff31da49141e8d525914ae5c8da
Paul <jamespauloconnor@gmail.com>
2015-09-02 18:13:18 -0400

    Got notes working properly.

--- ElevenNote.Web/Controllers/NotesController.cs
+++ ElevenNote.Web/Controllers/NotesController.cs
@@ -1,44 +1,66 @@
 ﻿using ElevenNote.Models.ViewModels;
+using ElevenNote.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace ElevenNote.Web.Controllers
 {
+
+    /// <summary>
+    /// The Authorize thing makes it so that you have to be logged in to see a note.
+    /// </summary>
+    [Authorize]
     public class NotesController : Controller
     {
         // GET: Notes
         public ActionResult Index()
         {
-            var notes = new List<NoteListViewModel>(); 
-            notes.Add(new NoteListViewModel()
-            {
-                Id = 0,
-                DateCreated = DateTime.UtcNow.AddMonths(-1),
-                DateModified = DateTime.UtcNow,
-                IsFavorite = true,
-                Title = "Some note title"
-            });
-            notes.Add(new NoteListViewModel()
-            {
-                Id = 0,
-                DateCreated = DateTime.UtcNow.AddMonths(-2),
-                DateModified = DateTime.UtcNow,
-                IsFavorite = true,
-                Title = "Another Title"
-            });
-            notes.Add(new NoteListViewModel()
-            {
-                Id = 0,
-                DateCreated = DateTime.UtcNow.AddMonths(-2),
-                DateModified = null,
-                IsFavorite = false,
-                Title = "Still, yes, another note"
-            });
-            
+            if (TempData["Result"] != null)
+            { ViewBag.Success = TempData["Result"];
+            TempData.Remove("Result");
+            }
+            var noteService = new NoteService();
+            var notes = noteService.GetAllForUser(Guid.Parse(User.Identity.GetUserId()));
             return View(notes);
         }
+
+        // GET: Note
+        [HttpGet]
+        [ActionName("Create")]
+        public ActionResult CreateGet()
+        {
+            return View();
+        }
+
+        // POST: Note
+        [HttpPost]
+        [ActionName("Create")]
+        public ActionResult CreatePost(NoteEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var noteService = new NoteService();
+                var userId = Guid.Parse(User.Identity.GetUserId());
+                var result = noteService.Create(model, userId);
+                TempData.Add("Result", result ? "Note added." : "Note not added.");
+                return RedirectToAction("Index");
+            }
+            return View(model);
+
+
+        }
+       // [HttpPost]
+        // [ActionName("Edit")]
+        // public ActionResult EditPost(NoteEditViewModel model)
+        //{
+          //  var editNote = new NoteService();
+            //editNote.Update();
+
+        //}
+
     }
 }
